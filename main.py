bf38d84e5ac57135df15856e16d09cd8ecda8b99
issuebombom <issuebombom@gmail.com>
2023-01-23 15:55:18 +0900

    Chore: minor

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from utils import YoutubeAudioExtractor
 
-def main():
-
+def main(url_list, concat=False):
+    '''
     url_list = [
-
         "https://youtu.be/YYOUHVJPukQ", 
         "https://youtu.be/xTZHYA4Asi0",
         "https://youtu.be/QM-vx1lrLaY"
-        
         ] # examples
+    '''
     youtube = YoutubeAudioExtractor(url_list)
-    audio_path_list = youtube.extract(concat_audio=True)
-
-    print(audio_path_list)
+    youtube.extract(concat_audio=concat)
 
 if __name__ == '__main__':
     main()
---
+++ run.sh
@@ -0,0 +1,2 @@
+source .venv/bin/activate
+streamlit run youtube_extractor_app.py
